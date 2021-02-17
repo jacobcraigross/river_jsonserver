fe625aab2a324aee5aa13cb27d91df47efb9557a
admin <admin@admins-MBP.attlocal.net>
2021-02-17 12:19:47 -0600

    01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const river = "brazos";
+  const creek = false
+
+  // video @ 27:55 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // will only return one parent element. (the div)
+    <div className="container">
+      <h1>hello from react</h1>
+      <p>{river}</p>
+      <p>{creek ? 'the creek is true' : 'no creek listed'}</p>
+      <Header title="Brazos and Trinity River" />
+      <p>some more text</p>
+      <button>click</button>
     </div>
+
+    // cannot put an H1 or p tag here, must go in the parent element above (the div in this case)
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = (props) => {
+    return (
+        <header>
+            <h1>river tracker</h1>
+            <h2>{props.title}</h2>
+        </header>
+    )
+}
+
+export default Header;
